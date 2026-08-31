320e0aeebd51b736bb87830207429b380e53710b
Divyanshuman Mandal <139416045+Kryptoknight24@users.noreply.github.com>
2026-08-31 23:36:55 +0530

    Time: 2125 ms (5.38%), Space: 7.8 MB (54.88%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        if(sum==num){
+            return true;
+        }
+        else{return false;}
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
